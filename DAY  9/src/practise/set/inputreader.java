f7b1ba1433d487e21fa7c79b5c52c731a744e165
Lenovo <Lenovo@LAPTOP-T0GISNTD>
2022-04-20 16:52:31 +0530

    inputreader

---
+++ DAY  9/src/practise/set/inputreader.java
@@ -0,0 +1,17 @@
+package practise.set;
+import java.io.*;
+public class inputreader {
+
+	public static void main(String[] args) throws IOException 
+	{
+		       InputStreamReader is=new InputStreamReader (System.in) ;
+	        System.out.println ("Enter number ");
+	       
+	        BufferedReader br=new BufferedReader (is);
+	        int num= Integer.parseInt (br.readLine());
+	        System.out.println ("you have entered" +num);
+	        
+
+	}
+
+}
